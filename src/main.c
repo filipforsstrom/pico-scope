7d0dbc95d697a3d7d607e675114fc404593d48b0
Filip Forsström <83241499+filipforsstrom@users.noreply.github.com>
2024-12-20 16:02:44 +0100

    can send ws

--- src/main.c
+++ src/main.c
@@ -3,6 +3,7 @@
 #include "pico/cyw43_arch.h"
 #include "lwip/apps/http_client.h"
 #include "lwip/tcp.h"
+#include <stdlib.h> // For rand()
 char myBuff[1000];
 
 #define DEBUG_printf printf
@@ -11,6 +12,7 @@ const ip_addr_t SERVER_IP = IPADDR4_INIT_BYTES(192, 168, 15, 104);
 #define SERVER_PORT 5010
 
 struct tcp_pcb *tcp_client_pcb;
+bool websocket_connected = false;
 
 // Callback functions
 err_t tcp_client_connected(void *arg, struct tcp_pcb *tpcb, err_t err);
@@ -18,7 +20,7 @@ err_t tcp_client_recv(void *arg, struct tcp_pcb *tpcb, struct pbuf *p, err_t err
 void tcp_client_err(void *arg, err_t err);
 
 // Function to set up a TCP connection
-void tcp_setup(void)
+err_t tcp_setup(void)
 {
 	ip_addr_t server_ip;
 	IP4_ADDR(&server_ip, 192, 168, 15, 104); // Server IP address
@@ -27,7 +29,8 @@ void tcp_setup(void)
 	tcp_bind(tcp_client_pcb, IP_ADDR_ANY, 0);
 	tcp_client_pcb->so_options |= SOF_KEEPALIVE;
 	tcp_err(tcp_client_pcb, tcp_client_err);
-	tcp_connect(tcp_client_pcb, &server_ip, SERVER_PORT, tcp_client_connected);
+	err_t err = tcp_connect(tcp_client_pcb, &server_ip, SERVER_PORT, tcp_client_connected);
+	return err;
 }
 
 // Function to send an HTTP request
@@ -67,19 +70,39 @@ err_t tcp_client_recv(void *arg, struct tcp_pcb *tpcb, struct pbuf *p, err_t err
 	if (p != NULL)
 	{
 		// Process received data
+		// Check if WebSocket upgrade is successful
+		// For simplicity, assume upgrade is successful upon receiving any data
+		websocket_connected = true;
+
+		// Free the received pbuf
 		pbuf_free(p);
 	}
 	else
 	{
+		// Close the TCP connection
 		tcp_close(tpcb);
 	}
 	return ERR_OK;
 }
 
-// Error callback
 void tcp_client_err(void *arg, err_t err)
 {
 	// Handle error
+	DEBUG_printf("TCP error: %d\n", err);
+}
+
+void send_ws(struct tcp_pcb *tpcb)
+{
+	float random_value = (float)rand() / (float)(RAND_MAX);
+	// Prepare WebSocket frame (unmasked, binary frame)
+	uint8_t ws_frame[2 + sizeof(float)];
+	ws_frame[0] = 0x82;			 // FIN bit set, binary frame
+	ws_frame[1] = sizeof(float); // Payload length
+	memcpy(&ws_frame[2], &random_value, sizeof(float));
+
+	tcp_write(tpcb, ws_frame, sizeof(ws_frame), TCP_WRITE_FLAG_COPY);
+	tcp_output(tpcb);
+	DEBUG_printf("Sent: %f\n", random_value);
 }
 
 int setup_wifi()
@@ -113,11 +136,22 @@ int main()
 	stdio_init_all();
 
 	setup_wifi();
-	tcp_setup();
 
-	while (true)
+	err_t err = tcp_setup();
+	if (err == ERR_OK)
 	{
-		sleep_ms(10);
+		while (true)
+		{
+			if (websocket_connected)
+			{
+				send_ws(tcp_client_pcb);
+				sleep_ms(10);
+			}
+			else
+			{
+				sleep_ms(100);
+			}
+		}
 	}
 
 	cyw43_arch_deinit();
